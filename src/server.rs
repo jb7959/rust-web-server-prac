1412f5ceb79f049b2f5cf246c1ec4749d81e031e
jay ahn <jb7959@naver.com>
2023-01-16 23:55:17 +0900

    noting

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ pub struct Server {
 
 // 구조체에 대한 생성자를 만들었다.
 impl Server {
-    //fn new (addr: String) -> Server  대신에 Self로 표현이 가능하다.
+    //fn new (addr: String) -> Server  대신에 Self 로 표현이 가능하다.
     pub fn new(addr: String) -> Self {
         Self {
             //addr: addr 를 줄여서 표현가능하다.
