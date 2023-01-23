6723651b0696073aba9cd0fc0f8c6edc59a403d9
jay ahn <jb7959@naver.com>
2023-01-23 20:44:41 +0900

    save

--- src/http/request.rs
+++ src/http/request.rs
@@ -18,13 +18,29 @@ impl TryForm<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
 
         let request = str::from_utf8(buf)?;
+
+/*        match get_next_word(request) {
+            Some((method, request)) => {},
+            None => return Err(ParseError::InvalidRequest),
+        }
+*/
+        // 로컬 변수 request 이름을 새로 할당했다. 이를 쉐도잉이라고 한다.
+        //마지막에 물음표를 안넣으면 예외발생시 강제종료된다.
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
         unimplemented!()
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' '{
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + 1..]));
         }
     }
