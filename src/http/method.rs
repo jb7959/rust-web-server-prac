3bfa7af50fa5cedb8f448b8dfba8cf88c660ec2b
jay ahn <jb7959@naver.com>
2023-01-24 09:37:19 +0900

    save

--- src/http/method.rs
+++ src/http/method.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 // enum은 각 순서마다 Default는 메모리에 0부터 1씩 증가하며 값을 할당한다.
 // pub가 아니면 private 다.
 pub enum Method {
@@ -11,3 +13,24 @@ pub enum Method {
     TRACE,
     PATCH,
 }
+
+impl FromStr for Method {
+    type Err = MethodError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err>{
+        match s {
+            "GET" => OK(Self::GET),
+            "DELETE" => OK(Self::DELETE),
+            "POST" => OK(Self::POST),
+            "PUT" => OK(Self::PUT),
+            "HEAD" => OK(Self::PUT),
+            "CONNECT" => OK(Self::CONNECT),
+            "OPTIONS" => OK(Self::OPTIONS),
+            "TRACE" => OK(Self::TRACE),
+            "PATCH" => OK(Self::PATCH),
+            _ => Err(MethodError),
+        }
+    }
+}
+
+pub struct MethodError;
--- src/http/request.rs
+++ src/http/request.rs
@@ -1,4 +1,4 @@
-use super::method::Method;
+use super::method::{Method, MethodError};
 use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt:: {Display, Debug, Formatter, Result as FmtResult};
@@ -16,23 +16,43 @@ pub struct Request {
 impl TryForm<&[u8]> for Request {
     type Error = String;
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
-
         let request = str::from_utf8(buf)?;
 
-/*        match get_next_word(request) {
-            Some((method, request)) => {},
-            None => return Err(ParseError::InvalidRequest),
-        }
-*/
-        // 로컬 변수 request 이름을 새로 할당했다. 이를 쉐도잉이라고 한다.
-        //마지막에 물음표를 안넣으면 예외발생시 강제종료된다.
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
             return Err(ParseError::InvalidProtocol);
         }
+
+        // 1. match 쓰려고하나 None의 경우 필요가 없음에도 선언해야함 (불필요한 빈매치)
+        let method: Method = method.parse()?;
+        let mut query_string = None;
+        match path.find('?') {
+            Some(i) => {
+                query_string = Some(&path[i + 1..]);
+                path = &path[..i]
+            }
+            None => {
+
+            }
+        }
+
+        // 2. match 를 사용하지 않으면 if가 등장함 + 불필요한 변수 필요
+        let q = path.find('?');
+        if(q.is_some()){
+            let i = q.unwrap();
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i]
+        }
+
+        // 3. rust에는 if let이라는 것이 있음, 불필요한 매치 제거 및 변수도 제거, 깔끔해짐
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i]
+        }
+
 
         unimplemented!()
     }
@@ -66,11 +86,16 @@ impl ParseError {
     }
 }
 
+impl From<MethodError> for ParseError{
+    fn from(_: MethodError) -> Self {
+        Self::InvalidEncoding
+    }
+}
+
 impl From<Utf8Error> for ParseError{
     fn from(_: Utf8Error) -> Self {
         Self::InvalidEncoding
     }
-    
 }
 
 impl Display for ParseError {
